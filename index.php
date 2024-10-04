b2f0f51fba741e926873df6046d0f02ab0322713
stellarew <infinitestarlightjourneyseeker@gmail.com>
2024-10-04 09:23:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <main>
     <h2>Selamat datang di Rental Mobil</h2>
     <p>Temukan mobil impian Anda di sini.</p>
-    <a href="../html/register.html" class="button">Register Customer</a>
-    <a href="../processes/register_admin.php" class="button">Register Admin</a>
+    <a href="html/register.html" class="button">Register Customer</a>
+    <a href="processes/register_admin.php" class="button">Register Admin</a>
 </main>
 <?php include('footer.php'); ?>
